60784d27a3cd25dba7b22ff4df94316ad4c2fa81
Pedro Henrique Zaccaria <pedrozaccaria@gmail.com>
2024-06-25 18:41:01 -0300

    refactor(path): namespace add

--- restapp/urls.py
+++ restapp/urls.py
@@ -1,3 +1,4 @@
+
 """
 URL configuration for webrest project.
 
@@ -14,10 +15,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from restapp.views import Home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('restapp.urls'))
+    path('', Home.as_view())
 ]
--- restapp/views.py
+++ restapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.views import View
 
 # Create your views here.
@@ -7,9 +7,9 @@ class Home(View):
     def get(self, request: HttpRequest):
         print('aq foi get')
 
-        return render(request, 'HOMEZADA')
+        return render(request, 'restapp/home.html')
     
     def post(self, request: HttpRequest):
         print('aq foi post')
 
-        return redirect('/')
+        return redirect('')
